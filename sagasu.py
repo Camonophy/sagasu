64f8ede367f44a11ae61180c6d53434700995874
pflugoli <pflugoli@b-tu.de>
2021-10-07 16:59:27 +0200

    Remove arbitrary if statement

--- sagasu.py
+++ sagasu.py
@@ -3,6 +3,8 @@
 import sys
 import subprocess
 
+from binaryornot.check import is_binary
+
 print()
 args = sys.argv[1:len(sys.argv)]    # Search terms
 
@@ -13,17 +15,19 @@ output = output[2:len(output)-3].split("\\n")
 while output != []:
     file = output.pop(0)
     ls = str(subprocess.check_output(["ls", file]))
-    if(ls[2:len(ls)-3] == file):                # Check, whether it is a file
+    if(ls[2:len(ls)-3] == file and not(is_binary(file))):                # Check, whether it is a file
         for arg in args:
             try:
-                found = str(subprocess.check_output(["grep", arg, file]))
-                print("| " + file + " |")
+                found = str(subprocess.check_output(["grep", "-n", arg, file]))
+                print("#### " + file + " ####")
                 lines = found[2:len(found)-3].split("\\n")
                 for line in lines:
                     print("\t"+line)
                 print()
             except:
                 continue
+    elif(is_binary(file)):
+        continue
     else:                                       # Extract dir
         for el in ls[2:len(ls)-3].split("\\n"):
             output.append(file+"/"+el)
